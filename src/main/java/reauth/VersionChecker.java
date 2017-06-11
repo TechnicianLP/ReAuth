0d5dc77bda9aa7c534d75341eba0b39d0075cc3f
TechnicianLP <technicianlp@users.noreply.github.com>
2017-06-12 00:22:31 +0200

    3.5.0 Update for 1.11

--- src/main/java/reauth/VersionChecker.java
+++ src/main/java/reauth/VersionChecker.java
@@ -1,54 +1,73 @@
 package reauth;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import net.minecraftforge.common.ForgeModContainer;
 import net.minecraftforge.fml.common.versioning.ComparableVersion;
 import net.minecraftforge.fml.common.versioning.DefaultArtifactVersion;
 import net.minecraftforge.fml.common.versioning.VersionParser;
 import net.minecraftforge.fml.common.versioning.VersionRange;
 import org.apache.commons.io.IOUtils;
 
-import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 class VersionChecker implements Runnable {
 
-    private static final String url = "https://raw.githubusercontent.com/TechnicianLP/ReAuth/master/version";
+    private static final String url = "https://raw.githubusercontent.com/TechnicianLP/ReAuth/master/version.json";
+    private static final String group = "1.11";
 
     private static boolean isLatestVersion = true;
-    private static String[] versionInfo = new String[]{"", ""};
+    private static String latestMessage = (char) 167 + "aUpdate Available!";
     private static boolean isVersionAllowed = true;
+    private static String allowedMessage = (char) 167 + "4Critical Update Available!";
+    private static boolean forgeCheck = true;
     private static long run = 0;
 
     @Override
     public void run() {
         Main.log.info("Looking for Updates");
-        InputStream in;
+        // is the forge checker enabled?
+        forgeCheck = ForgeModContainer.getConfig().get(ForgeModContainer.VERSION_CHECK_CAT, "Global", true).getBoolean();
+        forgeCheck &= ForgeModContainer.getConfig().get(ForgeModContainer.VERSION_CHECK_CAT, Main.meta.modId, true).getBoolean();
+        InputStream in = null;
         try {
             in = new URL(url).openStream();
-        } catch (Exception e) {
-            Main.log.error("Looking for Updates - Failed", e);
-            return;
-        }
+            JsonObject json = new JsonParser().parse(new InputStreamReader(in, "UTF-8")).getAsJsonObject();
+            if (json.has(group)) {
+                JsonObject data = json.getAsJsonObject(group);
+                String latest = data.getAsJsonPrimitive("latest").getAsString();
+                latestMessage = (char) 167 + "aUpdate Available!";
+                if (data.has("latest-message"))
+                    latestMessage += " - " + data.getAsJsonPrimitive("latest-message").getAsString();
+                String allowed = null;
+                if (data.has("allowed"))
+                    allowed = data.getAsJsonPrimitive("allowed").getAsString();
+                allowedMessage = (char) 167 + "4Critical Update Available!";
+                if (data.has("allowed-message"))
+                    allowedMessage += " - " + data.getAsJsonPrimitive("allowed-message").getAsString();
 
-        try {
-            versionInfo = IOUtils.readLines(in).get(0).split(";");
-
-            ComparableVersion local = new ComparableVersion(Main.meta.version);
-            ComparableVersion recommended = new ComparableVersion(versionInfo[0]);
-            VersionRange allowed = VersionParser.parseRange(versionInfo[1]);
-
-            isLatestVersion = local.compareTo(recommended) >= 0;
-            isVersionAllowed = allowed.containsVersion(new DefaultArtifactVersion(Main.meta.version));
+                ComparableVersion rLocal = new ComparableVersion(Main.meta.version);
+                ComparableVersion rRecommended = new ComparableVersion(latest);
+                isLatestVersion = rLocal.compareTo(rRecommended) >= 0;
+                if (allowed == null) {
+                    isVersionAllowed = true;
+                } else {
+                    VersionRange rAllowed = VersionParser.parseRange(allowed);
+                    isVersionAllowed = rAllowed.containsVersion(new DefaultArtifactVersion(Main.meta.version));
+                }
+            } else {
+                Main.log.error("Looking for Updates - Failed: Unknown Version: \"" + group + "\"");
+            }
             run = System.currentTimeMillis();
-
             Main.log.info("Looking for Updates - Finished");
             return;
-        } catch (IOException e) {
+        } catch (Exception e) {
             Main.log.error("Looking for Updates - Failed", e);
         } finally {
             IOUtils.closeQuietly(in);
         }
-
         run = System.currentTimeMillis() - (long) ((1000 * 60 * 60) * 0.75);
     }
 
@@ -56,14 +75,24 @@ class VersionChecker implements Runnable {
         return isLatestVersion;
     }
 
-    static String getLatestVersion() {
-        return versionInfo[0];
-    }
-
     static boolean isVersionAllowed() {
         return isVersionAllowed;
     }
 
+    /**
+     * returns update-message (hidden if the forge-version-checker is disabled)(allowed-checker unaffected)
+     */
+    static String getUpdateMessage() {
+        if (!isVersionAllowed)
+            return allowedMessage;
+        if (!isLatestVersion && forgeCheck)
+            return latestMessage;
+        return null;
+    }
+
+    /**
+     * should the check run? true if last check is older than 60 minutes (or 15 minutes if the last check failed)
+     */
     static boolean shouldRun() {
         return System.currentTimeMillis() - run > (1000 * 60 * 60);
     }
