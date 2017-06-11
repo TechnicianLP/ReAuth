99a688d6c693cb523ba41fa6403d8621a98bfb19
TechnicianLP <technicianlp@users.noreply.github.com>
2017-06-11 16:36:55 +0200

    1.12 Port + Changed VersionChecker

--- src/main/java/reauth/GuiFactory.java
+++ src/main/java/reauth/GuiFactory.java
@@ -12,6 +12,16 @@ public class GuiFactory implements IModGuiFactory {
     }
 
     @Override
+    public boolean hasConfigGui() {
+        return true;
+    }
+
+    @Override
+    public GuiScreen createConfigGui(GuiScreen parentScreen) {
+        return new ConfigGUI(parentScreen);
+    }
+
+    @Override
     public Class<? extends GuiScreen> mainConfigGuiClass() {
         return ConfigGUI.class;
     }
